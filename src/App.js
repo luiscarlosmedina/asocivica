37d8c5a28b895f981795425e861f58630084bded
Faebb <faeb265@gmail.com>
2023-06-20 16:40:39 -0500

    commit inicio de sesion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <div className="wrapper">
-        <h1>Application</h1>
         <BrowserRouter>
           <Routes>
             <Route path="/home" element={<Dashboard />} />
--- src/components/signIn/SignIn.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SignIn() {
-  return <h2>Esto es el Login</h2>;
-}
