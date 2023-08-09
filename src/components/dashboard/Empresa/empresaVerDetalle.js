c60cee4ad877c1b8f9c6bfc48fc8953eff69221c
luismedina <luiscarlosmedina481@gmail.com>
2023-08-09 09:35:00 -0500

    coneccion a api

---
+++ src/components/dashboard/Empresa/empresaVerDetalle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UseFetchPost } from "../useFetchPost"
+
+export default function EmpresaVerDetalle() {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
