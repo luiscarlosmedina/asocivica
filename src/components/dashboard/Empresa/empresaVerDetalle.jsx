321dd05eb0a94f69aeeb9e94fa016d57e0087e4b
luismedina <luiscarlosmedina481@gmail.com>
2023-08-24 10:46:27 -0500

    v3.0

--- src/components/dashboard/Empresa/empresaVerDetalle.jsx
+++ src/components/dashboard/Empresa/empresaVerDetalle.jsx
@@ -11,7 +11,7 @@ export default function EmpresaVerDetalle() {
   const handlePostData = async () => {
 
     const response = await fetch(
-      "http://developersaurios.000webhostapp.com/api.php?apicall=readidempresa",
+      "https://developersaurios.000webhostapp.com/api.php?apicall=readidempresa",
       {
         method: "POST",
         headers: {
