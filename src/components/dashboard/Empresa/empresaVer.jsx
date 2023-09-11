689d33b2b661a606c0951a62b5d22d36b9f8ef36
luismedina <luiscarlosmedina481@gmail.com>
2023-09-11 18:00:28 -0500

    editar sede

--- src/components/dashboard/Empresa/empresaVer.jsx
+++ src/components/dashboard/Empresa/empresaVer.jsx
@@ -1,70 +1,136 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  CircularProgress,
+  Button,
+  TextField,
+  TablePagination,
+} from "@mui/material";
 
 export default function EmpresaVer({ dataUpdated }) {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        fetchData();
-    }, []); // Agregar dataUpdated como dependencia
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = () => {
-        fetch(`http://localhost/api_proyecto.github.io/api.php?apicall=readempresa&id`)
-            .then((response) => response.json())
-            .then((data) => {
-                setData(data.contenido);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.log(error);
-                setLoading(false);
-            });
-    };
-    let id = "";
-    return (
-        <div className="consultar-container">
-            <h3>Empresas</h3>
-            <table className="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Nit</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Correo</th>
-                        <th scope="col">Telefono</th>
-                        <th scope="col">Representante</th>
-                        <th scope="col">T Doc</th>
-                        <th scope="col">N° Documento</th>
-                        <th scope="col">Estado</th>
-                        <th scope="col">Detalles</th>
-                    </tr>
-                </thead>
-                <tbody className="table-group-divider">
-                    {loading ? (
-                        <tr>
-                            <td className="spinner-border text-primary" role="status">
-                                <span className="visually-hidden">Cargando...</span>
-                            </td>
-                        </tr>
-                    ) : Array.isArray(data) ? (
-                        data.map(item => (
-                            <tr key={item.id_e}>
-                                <th scope="row">{item.Nit_E}</th>
-                                <td>{item.Nom_E}</td>
-                                <td>{item.Eml_E}</td>
-                                <td>{item.telefonoGeneral}</td>
-                                <td>{item.Nom_Rl}</td>
-                                <td>{item.N_TDoc}</td>
-                                <td>{item.CC_Rl}</td>
-                                <td>{item.Est_E === "0" ? "Activo" : item.Est_E === "1" ? "En estudio" : "Inactivo"}</td>
-                                <td><Link to={id = item.id_e}><button value={id} type="button" className="btn btn-primary">Ver mas</button></Link></td>
-                            </tr>
-                        ))
-                    ) : (
-                        <p>No hay datos disponibles</p>
-                    )}
-                </tbody>
-            </table>
-        </div>
-    )
+  const fetchData = () => {
+    fetch(`http://localhost/api_proyecto.github.io/api.php?apicall=readempresa&id`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.contenido);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+
+  const handleChangePage = ( newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const filteredData = data.filter(
+    (item) =>
+      item.Nom_E.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.Nit_E.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="consultar-container">
+      <h3>Empresas</h3>
+      <TextField
+        label="Buscar"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nit</TableCell>
+              <TableCell>Nombre</TableCell>
+              <TableCell>Correo</TableCell>
+              <TableCell>Telefono</TableCell>
+              <TableCell>Representante</TableCell>
+              <TableCell>N° Documento</TableCell>
+              <TableCell>Estado</TableCell>
+              <TableCell>Detalles</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {loading ? (
+              <TableRow>
+                <TableCell colSpan={9} align="center">
+                  <CircularProgress color="primary" />
+                </TableCell>
+              </TableRow>
+            ) : filteredData.length > 0 ? (
+              filteredData
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((item) => (
+                  <TableRow key={item.id_e}>
+                    <TableCell>{item.Nit_E}</TableCell>
+                    <TableCell>{item.Nom_E}</TableCell>
+                    <TableCell>{item.Eml_E}</TableCell>
+                    <TableCell>{item.telefonoGeneral}</TableCell>
+                    <TableCell>{item.Nom_Rl}</TableCell>
+                    <TableCell>{item.CC_Rl}</TableCell>
+                    <TableCell>
+                      {item.Est_E === "0"
+                        ? "Activo"
+                        : item.Est_E === "1"
+                        ? "En estudio"
+                        : "Inactivo"}
+                    </TableCell>
+                    <TableCell>
+                      <Link to={item.id_e}>
+                        <Button variant="contained" color="primary">
+                          Ver mas
+                        </Button>
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={9} align="center">
+                  No hay datos disponibles
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 50]}
+          component="div"
+          count={filteredData.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </TableContainer>
+    </div>
+  );
 }
--- src/components/dashboard/Empresa/sede.jsx
+++ src/components/dashboard/Empresa/sede.jsx
@@ -1,113 +1,210 @@
-import React, { useEffect, useState, Fragment } from 'react';
-import Encargados from './encargados';
-import Editar_s from './editarsede';
+import React, { useEffect, useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  CircularProgress,
+  Accordion,
+  AccordionDetails,
+  Button,
+  TextField,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import EditarSede from './editarsede'; // Importa tu componente EditarSede aquí
+import Encargados from './encargados'; // Importa tu componente Encargados aquí
 
 export default function Sede({ id }) {
-    const [data, setData] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [ids, setIds] = useState(null)
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [ids, setIds] = useState(null);
+  const [editing, setEditing] = useState({}); // Para rastrear las filas que se están editando
+  const [expandedSede, setExpandedSede] = useState(null); // Para rastrear la sede expandida
 
-    useEffect(() => {
-        fetchData();
-    }, []); // Agregar dataUpdated como dependencia
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = () => {
-        fetch(`http://localhost/api_proyecto.github.io/api.php?apicall=readsede&id=${id}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setData(data.contenido);
-                setLoading(false);
-                console.log(data);
-            })
-            .catch((error) => {
-                console.log(error);
-                setLoading(false);
-            });
-    };
-    const handleActual = (b)=>{
-        setIds(b)
-    }
-    return (
+  const fetchData = () => {
+    fetch(`http://localhost/api_proyecto.github.io/api.php?apicall=readsede&id=${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.contenido);
+        setLoading(false);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+
+  const handleActual = (b) => {
+    setIds(b);
+  };
+
+  const handleEdit = (id) => {
+    // Rastrea el estado de edición para la fila
+    setEditing({ ...editing, [id]: true });
+  };
+
+  const handleSaveLocally = (id, field, newValue) => {
+    // Obtiene el valor editado y el campo correspondiente
+    const updatedData = data.map((item) => {
+      if (item.ID_S === id) {
+        return { ...item, [field]: newValue };
+      }
+      return item;
+    });
+
+    setData(updatedData);
+  };
+
+  const handleSaveToAPI = (id, field, newValue) => {
+    // Obtiene el objeto de datos correspondiente
+    const editedRow = data.find((row) => row.ID_S === id);
+
+    // Actualiza el campo editado en el objeto de la fila
+    editedRow[field] = newValue;
+
+    // Envía los datos actualizados a la API
+    fetch(`http://localhost/api_proyecto.github.io/api.php?apicall=updatesede`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(editedRow),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        if (responseData) {
+          // Actualiza el estado de edición para esta fila
+          setEditing({ ...editing, [id]: false });
+          // Puedes realizar una nueva solicitud para actualizar los datos si es necesario
+          fetchData();
+        } else {
+          console.error('Error al actualizar los datos');
+        }
+      })
+      .catch((error) => {
+        console.error('Error al actualizar los datos:', error);
+      });
+  };
+
+  const handleExpandSede = (id) => {
+    // Rastrea qué sede está expandida
+    setExpandedSede(id === expandedSede ? null : id);
+  };
+
+  return (
+    <div>
+      <div className='d-flex justify-content-between align-items-center'>
         <div>
-            <div className='d-flex justify-content-between align-items-center'>
-                <div>
-                    <h3>Sedes y encargados</h3>
-                </div>
-                <div>
-                    <button type="button" className="btn btn-link" data-bs-toggle="modal" data-bs-target="#sede" data-bs-whatever="@mdo">Agregar sede </button>
-                </div>
-                {/* Modal */}
-                <div className="modal fade" id="sede" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <Editar_s id={id} />
-                </div>
-            </div>
-            <hr className='pb-3' />
-            <div className="container mt-4">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Direccion</th>
-                            <th scope="col">Sector de vigilancia</th>
-                            <th scope="col">Editar</th>
-                            <th scope="col">Detalles</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {loading ? (
-                            <tr>Cargando...</tr>
-                        ) : Array.isArray(data) ? (
-                            data.map(item => (
-                                <Fragment key={item.ID_S}>
-                                    <tr>
-                                        <td>{item.Dic_S}</td>
-                                        <td>{item.Sec_V}</td>
-                                        <td>
-                                            <div>
-                                                <button type="button" className="btn btn-link" data-bs-toggle="modal" data-bs-target="#edisede" data-bs-whatever="@mdo" onClick={ () => handleActual(item.ID_S) }>Editar </button>
-                                            </div>
-                                            {/* Modal */}
-                                            <div className="modal fade" id="edisede" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                                <Editar_s id={id} sid={ids} />
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <button
-                                                className="btn btn-primary"
-                                                type="button"
-                                                data-bs-toggle="collapse"
-                                                data-bs-target={`#collapse-${item.ID_S}`}
-                                                aria-expanded="false"
-                                                aria-controls={`collapse-${item.ID_S}`}
-                                            >
-                                                Ver encargados
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td colSpan="3">
-                                            <div className="accordion" id={`accordion-${item.ID_S}`}>
-                                                <div className="accordion-item">
-
-                                                    <div
-                                                        id={`collapse-${item.ID_S}`}
-                                                        className="accordion-collapse collapse"
-                                                        data-bs-parent={`#accordion-${item.ID_S}`}
-                                                    >
-                                                        <div className="accordion-body">
-                                                            <Encargados id={item.ID_S} />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </Fragment>
-                            ))) : (
-                            <p>No existen datos registrados</p>
-                        )}
-                    </tbody>
-                </table>
-            </div>
+          <h3>Sedes y encargados</h3>
         </div>
-    );
+        <div>
+          <button variant="outlined" color="primary" data-bs-toggle="modal" data-bs-target="#sede">
+            Agregar sede
+          </button>
+        </div>
+        {/* Modal */}
+        <div className="modal fade" id="sede" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <EditarSede id={id} />
+        </div>
+      </div>
+      <hr className='pb-3' />
+      <div className="container mt-4">
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Direccion</TableCell>
+                <TableCell>Sector de vigilancia</TableCell>
+                <TableCell>Mas opciones</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {loading ? (
+                <TableRow>
+                  <TableCell colSpan={3} align="center">
+                    <CircularProgress color="primary" />
+                  </TableCell>
+                </TableRow>
+              ) : Array.isArray(data) ? (
+                data.map((item) => (
+                  <React.Fragment key={item.ID_S}>
+                    <TableRow style={{ cursor: 'pointer' }}>
+                      <TableCell>
+                        {editing[item.ID_S] ? (
+                          <TextField
+                            fullWidth
+                            value={item.Dic_S}
+                            onChange={(e) => handleSaveLocally(item.ID_S, 'Dic_S', e.target.value)}
+                          />
+                        ) : (
+                          item.Dic_S
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        {editing[item.ID_S] ? (
+                          <TextField
+                            fullWidth
+                            value={item.Sec_V}
+                            onChange={(e) => handleSaveLocally(item.ID_S, 'Sec_V', e.target.value)}
+                          />
+                        ) : (
+                          item.Sec_V
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        {editing[item.ID_S] ? (
+                          <Button
+                            color="primary"
+                            onClick={() => handleSaveToAPI(item.ID_S, 'ID_S', item.ID_S)}
+                          >
+                            Guardar
+                          </Button>
+                        ) : (
+                          <Button
+                            color="primary"
+                            onClick={() => handleEdit(item.ID_S)}
+                          >
+                            Editar
+                          </Button>
+                        )}
+                        <Button variant="contained" color="info" onClick={() => handleExpandSede(item.ID_S)}>
+                          Encargados
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                    {expandedSede === item.ID_S && (
+                      <TableRow>
+                        <TableCell colSpan={3}>
+                          <Accordion expandIcon={<ExpandMoreIcon />}>
+                            <AccordionDetails>
+                              <Encargados id={item.ID_S} />
+                            </AccordionDetails>
+                          </Accordion>
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </React.Fragment>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={3} align="center">
+                    No existen datos registrados
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 }
