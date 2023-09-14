d39533b5ca57cb5205c3805ea85ebae9cef76a21
luismedina <luiscarlosmedina481@gmail.com>
2023-09-14 17:36:53 -0500

    editar encargado, en construccion

--- src/components/dashboard/Empresa/encargados.jsx
+++ src/components/dashboard/Empresa/encargados.jsx
@@ -14,16 +14,16 @@ import swal from 'sweetalert';
 
 export default function Encargados({ id }) {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [newEncargado, setNewEncargado] = useState({
     ID_S: id,
-    Est_en:"0",
+    Est_en: "0",
     N_En: "",
     tel1: "",
     tel2: "",
     tel3: "",
   });
   const [showInsertForm, setShowInsertForm] = useState(false);
+  const [editMode, setEditMode] = useState({});
 
   const fetchData = async () => {
     try {
@@ -31,17 +31,82 @@ export default function Encargados({ id }) {
         `http://localhost/api_proyecto.github.io/api.php?apicall=readTelSede&id=${id}`
       );
       const result = await response.json();
-      setData(result.contenido);
-      setLoading(false);
+      // Procesar los números de teléfono para separar ID y teléfono
+      const newData = result.contenido.map((item) => {
+        const telefonos = item.telefono.split(',');
+        const telefonosSeparados = telefonos.map((telefono, index) => {
+          const [id_tel, tel] = telefono.split('-');
+          return {
+            [`id_tel${index + 1}`]: id_tel,
+            [`tel${index + 1}`]: tel,
+          };
+        });
+        return {
+          ...item,
+          ...Object.assign({}, ...telefonosSeparados),
+        };
+      });
+      setData(newData);
     } catch (error) {
       console.error("Error:", error);
-      setLoading(false);
     }
   };
 
-  const deleteEnc = async (a) => {
+  const handleInputChange = (e, ID_En, telefonoIndex) => {
+    const { name, value } = e.target;
+    const newData = [...data];
+    const index = newData.findIndex((item) => item.ID_En === ID_En);
+    if (index !== -1) {
+      newData[index] = {
+        ...newData[index],
+        [`tel${telefonoIndex + 1}`]: value,
+      };
+      setData(newData);
+    }
+  };
+
+  const saveEnc = async (ID_En, telefonos) => {
+    // Enviar los datos actualizados al servidor y guardarlos
+    const updatedData = {
+      ID_En,
+      N_En: data.find((item) => item.ID_En === ID_En).N_En,
+      ...Object.assign({}, ...telefonos),
+    };
+    console.log(updatedData);
+    try {
+      const response = await fetch(`http://localhost/api_proyecto.github.io/api.php?apicall=updateencargado`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData),
+      });
+      const responseData = await response.json();
+
+      if (responseData) {
+        fetchData();
+        // Desactivar el modo de edición
+        setEditMode({
+          ...editMode,
+          [ID_En]: false,
+        });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const editeEnc = (ID_En) => {
+    // Activar el modo de edición para el encargado con ID_En
+    setEditMode({
+      ...editMode,
+      [ID_En]: true,
+    });
+  };
+
+  const deleteEnc = async (ID_En) => {
     const dt = {
-      ID_En: a,
+      ID_En,
       Est_en: '1'
     };
 
@@ -77,11 +142,6 @@ export default function Encargados({ id }) {
     }
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewEncargado({ ...newEncargado, [name]: value });
-  };
-
   const handleInsertSubmit = async () => {
     // Validación básica antes de enviar datos al servidor
     if (!newEncargado.N_En || !newEncargado.tel1) {
@@ -90,30 +150,29 @@ export default function Encargados({ id }) {
 
     try {
       const response = await fetch(`http://localhost/api_proyecto.github.io/api.php?apicall=createencargado`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newEncargado),
-        });
-        const responseData = await response.json();
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newEncargado),
+      });
+      const responseData = await response.json();
 
-        if (responseData) {
-          fetchData();
-          // Restablecer los campos del formulario después de la inserción
-          setNewEncargado({
-            ID_S: id,
-            N_En: "",
-            tel1: "",
-            tel2: "",
-            tel3: "",
-          });
-          setShowInsertForm(false);
-        }
-      } catch (error) {
-        console.error("Error:", error);
-        setLoading(false);
+      if (responseData) {
+        fetchData();
+        // Restablecer los campos del formulario después de la inserción
+        setNewEncargado({
+          ID_S: id,
+          N_En: "",
+          tel1: "",
+          tel2: "",
+          tel3: "",
+        });
+        setShowInsertForm(false);
       }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   useEffect(() => {
@@ -142,7 +201,7 @@ export default function Encargados({ id }) {
                     variant="outlined"
                     size="small"
                     value={newEncargado.N_En}
-                    onChange={handleInputChange}
+                    onChange={(e) => setNewEncargado({ ...newEncargado, N_En: e.target.value })}
                   />
                 </TableCell>
                 <TableCell>
@@ -152,7 +211,7 @@ export default function Encargados({ id }) {
                     variant="outlined"
                     size="small"
                     value={newEncargado.tel1}
-                    onChange={handleInputChange}
+                    onChange={(e) => setNewEncargado({ ...newEncargado, tel1: e.target.value })}
                   />
                 </TableCell>
                 <TableCell>
@@ -162,7 +221,7 @@ export default function Encargados({ id }) {
                     variant="outlined"
                     size="small"
                     value={newEncargado.tel2}
-                    onChange={handleInputChange}
+                    onChange={(e) => setNewEncargado({ ...newEncargado, tel2: e.target.value })}
                   />
                 </TableCell>
                 <TableCell>
@@ -172,7 +231,7 @@ export default function Encargados({ id }) {
                     variant="outlined"
                     size="small"
                     value={newEncargado.tel3}
-                    onChange={handleInputChange}
+                    onChange={(e) => setNewEncargado({ ...newEncargado, tel3: e.target.value })}
                   />
                 </TableCell>
                 <TableCell>
@@ -205,11 +264,82 @@ export default function Encargados({ id }) {
             {data.map((item) => (
               item.Est_en === "0" && (
                 <TableRow key={item.ID_En}>
-                  <TableCell>{item.N_En}</TableCell>
-                  <TableCell>{item.telefono.split(',')[0]}</TableCell>
-                  <TableCell>{item.telefono.split(',')[1]}</TableCell>
-                  <TableCell>{item.telefono.split(',')[2]}</TableCell>
                   <TableCell>
+                    {editMode[item.ID_En] ? (
+                      <TextField
+                        name="N_En"
+                        label="Nombre encargado"
+                        variant="outlined"
+                        size="small"
+                        value={item.N_En}
+                        onChange={(e) => handleInputChange(e, item.ID_En, 0)}
+                      />
+                    ) : (
+                      item.N_En
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {editMode[item.ID_En] ? (
+                      <TextField
+                        name="tel1"
+                        label="Telefono 1"
+                        variant="outlined"
+                        size="small"
+                        value={item.tel1}
+                        onChange={(e) => handleInputChange(e, item.ID_En, 0)}
+                      />
+                    ) : (
+                      item.tel1
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {editMode[item.ID_En] ? (
+                      <TextField
+                        name="tel2"
+                        label="Telefono 2"
+                        variant="outlined"
+                        size="small"
+                        value={item.tel2}
+                        onChange={(e) => handleInputChange(e, item.ID_En, 1)}
+                      />
+                    ) : (
+                      item.tel2
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {editMode[item.ID_En] ? (
+                      <TextField
+                        name="tel3"
+                        label="Telefono 3"
+                        variant="outlined"
+                        size="small"
+                        value={item.tel3}
+                        onChange={(e) => handleInputChange(e, item.ID_En, 2)}
+                      />
+                    ) : (
+                      item.tel3
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {editMode[item.ID_En] ? (
+                      <Button
+                        color="primary"
+                        onClick={() => saveEnc(item.ID_En, [
+                          { id_tel: item.id_tel1, tel: item.tel1 },
+                          { id_tel: item.id_tel2, tel: item.tel2 },
+                          { id_tel: item.id_tel3, tel: item.tel3 },
+                        ])}
+                      >
+                        Guardar
+                      </Button>
+                    ) : (
+                      <Button
+                        color="primary"
+                        onClick={() => editeEnc(item.ID_En)}
+                      >
+                        Editar
+                      </Button>
+                    )}
                     <Button
                       variant="outlined"
                       color="secondary"
