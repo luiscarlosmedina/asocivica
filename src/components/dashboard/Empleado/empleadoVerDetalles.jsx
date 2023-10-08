70d4f5d247b5681c8cf1acb5845da77848ecdeb1
luismedina <luiscarlosmedina481@gmail.com>
2023-10-08 14:29:53 -0500

    add diseño verempleadosdetalles

--- src/components/dashboard/Empleado/empleadoVerDetalles.jsx
+++ src/components/dashboard/Empleado/empleadoVerDetalles.jsx
@@ -26,16 +26,47 @@ export default function EmpleadoVerDetalles() {
                     <div className="row">
                         <div className="card border border-0 col-md-2">
                             <img src={usersPhoto('./pablo.jpg')} className="card-img-top" alt="Image by rawpixel.com on Freepik" />
-                                <div className="card-body">
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
+                            <div className="card-body">
+                                <p className="card-text">estado {/*item.tel_em*/}</p>
+                                <p className="card-text">rol {/*item.id_rol*/}</p>
+                                <p className="card-text">rh {/*item.id_rh*/}</p>
+                            </div>
                         </div>
-                        
+                        <div className='col-10'>
+                            <div className='row'>
+                                <div className='col-md-11'>
+                                    <div className='d-flex col justify-content-between align-items-center text-bs-gray fs-5 fw-semibold p-0 m-0'>
+                                        <p>nombre y apellido {/*item.n_em + " " + item.a_em*/}</p>
+                                        <p>tipo y num doc {/*item.id_doc + " " + item.documento*/}</p>
+                                    </div>
+                                    <hr className='p-0 m-0' />
+                                </div>
+                            </div>
+                            <div className='row my-3'>
+                                <div className='col-md-6 '>
+                                    <p>correo</p>
+                                    <p>direccion y barrio</p>
+                                    <p>telefono</p>
+                                    <p>contrato</p>
+                                    <p>licencia de conduccion</p>
+                                </div>
+                                <div className='col-md-6 '>
+                                    <p>libreta militar</p>
+                                    <p>pension</p>
+                                    <p>cesantias</p>
+                                    <p>eps</p>
+                                    <p>arl</p>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
             )}
-            {/* <ContactoEmergencia id={id} /> */}
+            <div>
+                <ContactoEmergencia id={id} />
+            </div>
         </div>
     )
 }
