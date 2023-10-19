f6fe54c2b02fe0abe7845ec14d0e7180582eacff
porrotimido <Johanmora200412@gmail.com>
2023-10-18 21:45:05 -0500

    el perrengue

--- src/components/dashboard/Empleado/Reg_emple/buttons.jsx
+++ src/components/dashboard/Empleado/Reg_emple/buttons.jsx
@@ -6,54 +6,49 @@ function Botones() {
     <div>
       <figure className="figure1">
         <blockquote class="blockquote">
-          <p>Una cita conocida, contenida en un elemento blockquote.</p>
+          <p>¡Bienvenido al proceso de incorporación de nuevos empleados!</p>
         </blockquote>
         <figcaption class="blockquote-footer">
-          Alguien fomoso en <cite title="Título fuente">Título fuente</cite>
+          <cite title="Título fuente">Por favor, complete este formulario con la información necesaria para agregar internamente a un nuevo miembro al sistema.</cite>
         </figcaption>
       </figure>
       <div className="box-main">
         <header className="primary-box">
           <div className="box-menu">
-            <button className="buton_uno sub-menu">
-              <div className="box-options">
-                <h5>Información empresa</h5>
-                <div className="circle-options">
-                </div>
+            <div className="sep1">
+              <div className=" sub-menu">
+                <i class="bi bi-person-fill-check"></i>
               </div>
-            </button>
-            <div className="sep1"></div>
-            <button className="buton_dos sub-menu">
-              <div className="box-options">
-                <h5>Información basica 1</h5>
-                <div className="circle-options">
-                </div>
+              <p className="ip">información Personal </p>
+            </div>
+            <div className="l_a"></div>
+            <div className="sep2">
+              <div className="sub-menu">
+                <i class="bi bi-person-fill-check"></i>
               </div>
-            </button>
-            <div className="sep2"></div>
-            <button className="buton_tres sub-menu">
-              <div className="box-options">
-                <h5>Información basica 2</h5>
-                <div className="circle-options">
-                </div>
+              <p className="ip">información basica 1</p>
+            </div>
+            <div className="l_b"></div>
+            <div className="sep3">
+              <div className="sub-menu">
+                <i class="bi bi-person-fill-check"></i>
               </div>
-            </button>
-            <div className="sep3"></div>
-            <button className="buton_cuatro sub-menu">
-              <div className="box-options">
-                <h5>Contactos emergencia</h5>
-                <div className="circle-options">
-                </div>
+              <p className="ip">información basica 2</p>
+            </div>
+            <div className="l_c"></div>
+            <div className="sep4">
+              <div className=" sub-menu">
+                <i class="bi bi-person-fill-check"></i>
               </div>
-            </button>
-            <div className="sep4"></div>
-            <button className="buton_cinco sub-menu">
-              <div className="box-options">
-                <h5>Contrato empleado</h5>
-                <div className="circle-options">
-                </div>
+              <p className="ip">contactos emergencia</p>
+            </div>
+            <div className="l_d"></div>
+            <div className="sep5">
+              <div className="sub-menu">
+                <i class="bi bi-person-fill-check"></i>
               </div>
-            </button>
+              <p className="ip">Contrato</p>
+            </div>
           </div>
         </header>
       </div>
--- src/components/dashboard/Empleado/Reg_emple/textemple.jsx
+++ src/components/dashboard/Empleado/Reg_emple/textemple.jsx
@@ -2,16 +2,11 @@ import React from "react";
 
 function Textemple() {
     return (
-        <div className="m-2 border-bottom border-primary border-3 row justify-content-between">
+        <div className="m-2 border-bottom  border-3 row justify-content-between" >
           <div className="col-">
-            <p className="text-primary h1 mb-3 ms-3 mt-4">Registrar Empleado en el sistema</p>
+            <p className="text-primary h1 mb-3 ms-3 mt-4"  >Registrar Empleado en el sistema</p>
           </div>
-          
         </div>
-        
-
-        
-
     );
 
 }
