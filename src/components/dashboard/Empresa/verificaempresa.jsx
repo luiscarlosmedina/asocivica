f657246f56917c066b90fbda6d27b817ae6ebba8
luismedina <luiscarlosmedina481@gmail.com>
2023-10-24 16:31:48 -0500

    feat validador de nit

--- src/components/dashboard/Empresa/verificaempresa.jsx
+++ src/components/dashboard/Empresa/verificaempresa.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Empresafr from './Empresafr'
 
 export default function Verificaempresa() {
     const [nit, setNit] = useState("")
     const [nitValido, setNitValido] = useState(true); // Inicialmente asumimos que el NIT es válido
     const [formulario, setFormulario] = useState(false);
+    const navega = useNavigate()
+
+    const fetchData = async (nit) => {
+        try {
+            const response = await fetch(`http://localhost/api_proyecto.github.io/api.php?apicall=readempresanit&nit=${nit}`);
+            const data = await response.json();
+
+            if (data.contenido !== null) {
+                return data.contenido;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (nitValido === true) {
+            try {
+                const contenido = await fetchData(nit);
+                if (contenido === null || contenido.length === 0) {
+                    setFormulario(true);
+                } else {
+                    const valor = contenido[0]
+                    return navega(`/consultar-empresas/${(valor.id_e)}`)
+                }
+            } catch (error) {
+                // Manejar el error de la solicitud fetch
+                console.error(error);
+            }
+        } else {
+            console.log("Ingrese un NIT válido");
+        }
+    };
 
     const handleChange = (e) => {
         const nuevoNit = e.target.value;
@@ -18,21 +57,6 @@ export default function Verificaempresa() {
 
         setNitValido(esNitValido);
     };
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const data = true
-
-        if (nitValido === true) {
-            if (data === true) {
-                setFormulario(true)
-            } else {
-                <p>hola</p>
-            }
-        } else {
-            <p>ingrese un NIT valido</p>
-        }
-    }
 
     return (
         <>
